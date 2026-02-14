f5bde46e8ad2222a77abd6f2eabb142b3fbe5cdc
an ga <rao2k42@gmail.com>
2026-02-14 16:11:33 -0500

    test

--- roboshop_01.sh
+++ roboshop_01.sh
@@ -11,7 +11,7 @@ instance_id  =$(aws ec2 run-instances\
                 --image-id $AMI_ID \
                 --instance-type t3.micro \
                 --security-group-ids $SG_ID \
-                 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'\
+                 --tag-specifications"ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
                 --query 'Instances[0].PrivateIPAddress'\
                 --query 'Instances[0].InstanceId'\
                 --output text )
