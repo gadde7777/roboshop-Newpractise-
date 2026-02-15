bd7a8fa7bdb6a08c03261500502839a38a792a16
an ga <rao2k42@gmail.com>
2026-02-14 22:18:27 -0500

    test

--- roboshop_01.sh
+++ roboshop_01.sh
@@ -17,9 +17,9 @@ do
 #                 )
 
      INSTANCE_ID=$(aws ec2 run-instances \
-                    --image-id ami-0220d79f3f480ecf5 \
+                    --image-id $AMI_ID \
                     --instance-type t3.micro \
-                    --security-group-ids sg-0d48b1166075f71bf \
+                    --security-group-ids $SG_ID \
                     --tag-specifications  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
                     --query 'Instances[0].InstanceId' \
                     --output text)
@@ -30,27 +30,18 @@ do
  if [ $instance == "frontend" ]; then
 
  IP=$(aws ec2 describe-instances \
- --instance-ids $INSTANCE_ID
+ --instance-ids $INSTANCE_ID\
  --query 'Reservations[].Instances[].PublicIpAddress'\
   --output text)
   else
 
    IP=$(aws ec2 describe-instances \
- --instance-ids $INSTANCE_ID
- --query 'Reservations[].Instances[].PrivateIpAddress'\
-  --output text)
+    --instance-ids $INSTANCE_ID\
+    --query 'Reservations[].Instances[].PrivateIpAddress'\
+    --output text)
 
  fi           
 
 done
 
 
-#      INSTANCE_ID=$(aws ec2 run-instances \
-#                     --image-id ami-0220d79f3f480ecf5 \
-#                     --instance-type t3.micro \
-#                     --security-group-ids sg-0d48b1166075f71bf \
-#                     --tag-specifications  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-#                     --query 'Instances[0].InstanceId' \
-#                     --output text)
-
-# echo "Launched Instance ID: $INSTANCE_ID"
