834d07d48fa4d7b23a23a1075fb7317166478079
an ga <rao2k42@gmail.com>
2026-02-14 22:14:20 -0500

    test

--- roboshop_01.sh
+++ roboshop_01.sh
@@ -3,9 +3,9 @@
     SG_ID="sg-0d48b1166075f71bf"
     AMI_ID="ami-0220d79f3f480ecf5"
 
-# for instance in $@
+for instance in $@
 
-# do
+do
 
 # INSTANCE_ID=$( aws ec2 run-instances \
 #                 --image-id $AMI_ID \
@@ -16,31 +16,41 @@
 #                 # --output text 
 #                 )
 
-
-
-#  if [ $instance == "frontend" ]; then
-
-#  IP=$(aws ec2 describe-instances \
-#  --instance-ids $INSTANCE_ID
-#  --query 'Reservations[].Instances[].PublicIpAddress'\
-#   --output text)
-#   else
-
-#    IP=$(aws ec2 describe-instances \
-#  --instance-ids $INSTANCE_ID
-#  --query 'Reservations[].Instances[].PrivateIpAddress'\
-#   --output text)
-
-#  fi           
-
-# done
-
-
      INSTANCE_ID=$(aws ec2 run-instances \
                     --image-id ami-0220d79f3f480ecf5 \
                     --instance-type t3.micro \
                     --security-group-ids sg-0d48b1166075f71bf \
+                    --tag-specifications  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
                     --query 'Instances[0].InstanceId' \
                     --output text)
 
-echo "Launched Instance ID: $INSTANCE_ID"
+ echo "Launched Instance ID: $INSTANCE_ID"
+
+
+ if [ $instance == "frontend" ]; then
+
+ IP=$(aws ec2 describe-instances \
+ --instance-ids $INSTANCE_ID
+ --query 'Reservations[].Instances[].PublicIpAddress'\
+  --output text)
+  else
+
+   IP=$(aws ec2 describe-instances \
+ --instance-ids $INSTANCE_ID
+ --query 'Reservations[].Instances[].PrivateIpAddress'\
+  --output text)
+
+ fi           
+
+done
+
+
+#      INSTANCE_ID=$(aws ec2 run-instances \
+#                     --image-id ami-0220d79f3f480ecf5 \
+#                     --instance-type t3.micro \
+#                     --security-group-ids sg-0d48b1166075f71bf \
+#                     --tag-specifications  "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+#                     --query 'Instances[0].InstanceId' \
+#                     --output text)
+
+# echo "Launched Instance ID: $INSTANCE_ID"
