937e5345b3ed1f81d126c2cd3109b0e9580cd628
an ga <rao2k42@gmail.com>
2026-02-17 19:31:04 -0500

    record

---
+++ mysql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws88straining.online
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+
+echo -e "$R Run with root access $N" | tee -a $LOGS_FILE
+exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE()
+{
+  if [ $1 -ne 0 ]; then
+  echo -e "$2.......$R FAILURE $N"| tee -a $LOGS_FILE
+  exit 1
+  else
+  echo -e "$2........$G SUCCESS $N"| tee -a $LOGS_FILE
+  fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MySQL"
+
+systemctl enable mysqld
+VALIDATE $? "Enabling Mysql"
+
+systemctl start mysqld  
+VALIDATE $? "Starting Mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Set Root password"
